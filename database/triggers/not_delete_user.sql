a0b5b883fea67b9cea9419e7c43c94bdacb45b8e
jasperhessellund <jasperhessellund@gmail.com>
2019-12-09 19:32:44 +0100

    Work in progress: trigger to check if a user has purchased

---
+++ database/triggers/not_delete_user.sql
@@ -0,0 +1,18 @@
+--TODO: Finnish dont delete trigger
+
+CREATE TRIGGER dbo.trg_dont_delete_user
+ON dbo.tUsers
+INSTEAD OF DELETE
+AS
+SET NOCOUNT ON
+IF(SELECT COUNT(*) FROM tUsers WHERE nTotalAmount >0) > 0
+
+DECLARE @ERROR_MESSAGE VARCHAR(500)
+SELECT @ERROR_MESSAGE		=
+	'Trigger trg_dont_delete_user - '+
+	'User has more than one purchase. Deletion is disabled'
+RAISERROR( @ERROR_MESSAGE, 16, 1)
+if @@trancount > 0 BEGIN ROLLBACK END
+
+RETURN
+GO
